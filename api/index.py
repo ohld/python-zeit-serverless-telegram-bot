e41c5384087c803ad2bfa425fe20ff4841281b5e
ohld <>
2019-10-15 16:42:19 +0300

    debug

--- api/index.py
+++ api/index.py
@@ -7,10 +7,13 @@ app = Bottle()
 
 @app.get('/api')
 def api():
-    bot = telegram.Bot(token=os.environ["telegram_token"])
+    TELEGRAM_TOKEN = os.environ["telegram_token"]
+    if not TELEGRAM_TOKEN:
+        return {"status": "error", "reason": "no tg token"}
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
-    return "ok"
+    return {"status": "ok"}
