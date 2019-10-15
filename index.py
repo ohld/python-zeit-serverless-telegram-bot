130ae25deaa7aecece80d8b2c2ce6476b7a0786e
ohld <>
2019-10-15 16:13:44 +0300

    init

---
+++ index.py
@@ -0,0 +1,16 @@
+import os
+import telegram
+
+from bottle import Bottle, request
+
+app = Bottle()
+
+@app.get('/api')
+def api():
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+        # Reply with the same message
+        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    return "ok"
