3b1db3b4501a1b3961ff27632908bca95fc75aca
ohld <>
2019-10-15 17:16:55 +0300

    fix

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
 import os
 import telegram
 
-from flask import Flask, Response
+from flask import Flask, Response, request
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
-@app.route('/app')
+@app.route('/api')
 def api():
     TELEGRAM_TOKEN = os.getenv("telegram_token")
     if not TELEGRAM_TOKEN:
-        return {"status": "error", "reason": "no tg token"}
+        return Response({"status": "error", "reason": "no tg token"})
         
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     if request.method == "POST":
@@ -20,4 +20,4 @@ def api():
         chat_id = update.message.chat.id
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
-    return {"status": "ok"}
+    return Response({"status": "ok"})
