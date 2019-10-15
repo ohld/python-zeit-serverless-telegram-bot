f91641bdf5d5d7f57d4e0ff183cc775f56e16a72
ohld <>
2019-10-15 18:39:55 +0300

    add methods

--- index.py
+++ index.py
@@ -5,22 +5,18 @@ import telegram
 from flask import Flask, jsonify, Response, request
 app = Flask(__name__)
 
-# @app.route('/', defaults={'path': ''})
-# def catch_all(path):
-#     return Response("<h1>Flask is working</h1>")
-
-@app.route('/api')
+@app.route('/api', methods=['GET', 'POST'])
 def api():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     if not TELEGRAM_TOKEN:
         return jsonify({"status": "error", "reason": "no tg token"})
         
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    logging.warning(f"Request: {request}")
 
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        
     return jsonify({"status": "ok"})
