74f20cbeaca5ac9aacb52cf527be71aa7e22483c
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2020-04-17 23:16:50 +0300

    Update index.py

--- index.py
+++ index.py
@@ -41,13 +41,11 @@ def getme():
 @app.route('/api', methods=['GET', 'POST'])
 def api():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    if not TELEGRAM_TOKEN:
+    if TELEGRAM_TOKEN is None:
         return jsonify({"status": "error", "reason": "no tg token"})
         
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(TELEGRAM_TOKEN)
     
-    print(bot.get_me())
-
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
