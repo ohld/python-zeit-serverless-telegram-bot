ab48d61a5d12c37e0afe1a79d9d8fc05dd3838fd
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2020-04-17 23:07:08 +0300

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,32 @@ import telegram
 from flask import Flask, jsonify, Response, request
 app = Flask(__name__)
 
+
+def generate_cats():
+    import random
+    
+    cs_all = [
+        "👊 Опасность",
+        "💪 Брутальность",
+        "⭐️ Заметность",
+        "⚡️ Актуальность",
+        "😍 Сексуальность",
+        "😱 Неординарность",
+        "🤙 Прошаренность",
+        "♻️ Экологичность",
+    ]
+    
+    cs = random.sample(cs_all, 3)
+    
+    t = f"""
+{c[0]}: {random.randint(4,6)}/5
+{c[1]}: {random.randint(3,6)}/5
+{c[2]}: {random.randint(1,6)}/5
+    """
+        
+    return t
+        
+
 @app.route('/api', methods=['GET', 'POST'])
 def api():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -12,11 +38,14 @@ def api():
         return jsonify({"status": "error", "reason": "no tg token"})
         
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    
+    print(bot.get_me())
 
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        text = generate_cats() # update.message.text
+        bot.sendMessage(chat_id=chat_id, text=text)
         
     return jsonify({"status": "ok"})
